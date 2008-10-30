295f9513a5b8121266e6bcd39ee265438853a866
rbarazi <rbarazi@gmail.com>
2008-10-30 19:02:32 +0400

    Generating conditions methods and adding list method

--- lib/simply_searchable.rb
+++ lib/simply_searchable.rb
@@ -7,11 +7,18 @@ module SpinBits
     
     module ClassMethods
       def simply_searchable(options = {})
-        include SearchMethods
+        class_inheritable_accessor :attrs
+        self.attrs = self.columns.collect{|c| [c.name, c.type]}
+        self.attrs.each do |attribute|
+          named_scope "where_#{attribute[0]}".to_sym, lambda {|value| { :conditions => ["#{attribute[0]} = ?", value] }}          
+        end
       end
       
       def list(options)
-        
+        self.attrs.each do |attribute|
+          listings = (listings || self).send("where_#{attribute[0]}".to_sym, options[:attribute[0].to_sym]) unless options[:attribute[0].to_sym].blank?
+        end
+        return (listings || self).paginate(:page => options[:page])
       end
     end # ClassMethods
   end # SimplySearchable
