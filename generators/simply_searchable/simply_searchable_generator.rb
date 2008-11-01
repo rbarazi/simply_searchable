7664102628085dfeb94e19b2ed4b453bf1426d57
rbarazi <rbarazi@gmail.com>
2008-11-02 02:42:48 +0400

    Removing generators from old version

--- generators/simply_searchable/simply_searchable_generator.rb
+++
@@ -1,7 +0,0 @@
-class SimplySearchableGenerator < Rails::Generator::NamedBase
-  def manifest
-    record do |m|
-      m.template "search.html.erb", "app/views/#{plural_name}/search.html.erb"
-    end
-  end
-end
