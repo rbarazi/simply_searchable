e1e43804da7908dfbbab4b978f3bddc2e3fd95c7
rbarazi <rbarazi@gmail.com>
2008-11-03 00:02:33 +0400

    will_paginate support is now optional

--- lib/simply_searchable.rb
+++ lib/simply_searchable.rb
@@ -20,8 +20,23 @@ module SpinBits
       #
       # For string and text attributes like title and body, 
       # named_scope methods will expect a text and wil match using '%string%'
+      #
+      # By default SimplySearchable uses will_paginate
+      # to disable will_paginate just pass: :will_paginate => false
+      #
+      #   class Post < ActiveRecord::Base
+      #     simply_searchable :will_paginate => false
+      #   end
+      #
+      #   class Post < ActiveRecord::Base
+      #     simply_searchable :per_page => 20
+      #   end
+      #
       def simply_searchable(options = {})
-        class_inheritable_accessor :attrs
+        options.reverse_merge!(:per_page => 30, :with_pagination => true)
+        class_inheritable_accessor :attrs, :with_pagination, :per_page
+        self.with_pagination = options[:with_pagination]
+        self.per_page = options[:per_page]
         self.attrs = self.columns.collect{|c| [c.name, c.type]}
         self.attrs.each do |attribute|
           case attribute[1]
@@ -38,13 +53,13 @@ module SpinBits
       # Post.list(:title => 'abc', :created_at => Date.today)
       # 
       # Will return the posts that contain 'abc' in their title and created today.
-      def list(options)
+      def list(options={})
         listings = self
         options.each_pair do |key, value|
           key = key.to_s
           listings = listings.send("where_#{key}".to_sym, value) unless value.blank? or !self.column_names.include?key
         end
-        return listings.paginate(:page => options[:page])
+        return Post.with_pagination ? listings.paginate(:page => options[:page], :per_page => options[:per_page]) : listings.all
       end
     end # ClassMethods
   end # SimplySearchable
